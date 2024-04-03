e30375f4c7ab70d6c799655d6cc5c6fe334eac28
pitbox <migriswo@iu.edu>
2024-04-03 10:53:44 -0400

    Fix #15

--- src/main/java/github/pitbox46/fightnbtintegration/mixins/EpicFightCapabilitiesMixin.java
+++ src/main/java/github/pitbox46/fightnbtintegration/mixins/EpicFightCapabilitiesMixin.java
@@ -2,36 +2,43 @@ package github.pitbox46.fightnbtintegration.mixins;
 
 import github.pitbox46.fightnbtintegration.Config;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import yesman.epicfight.world.capabilities.EpicFightCapabilities;
 import yesman.epicfight.world.capabilities.item.CapabilityItem;
 
 @Mixin(value = EpicFightCapabilities.class, remap = false)
 public class EpicFightCapabilitiesMixin {
-    @Inject(at = @At(value = "HEAD"), method = "getItemStackCapability", cancellable = true)
-    private static void onGetItemStackCapability(ItemStack stack, CallbackInfoReturnable<CapabilityItem> cir) {
-        if(stack.isEmpty()) {
-            cir.setReturnValue(CapabilityItem.EMPTY);
+    @Shadow @Final public static Capability<CapabilityItem> CAPABILITY_ITEM;
+
+    @Redirect(
+            method = "getItemStackCapability",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getCapability(Lnet/minecraftforge/common/capabilities/Capability;)Lnet/minecraftforge/common/util/LazyOptional;")
+    )
+    private static LazyOptional<CapabilityItem> getFromConfig(ItemStack instance, Capability<CapabilityItem> capability) {
+        CapabilityItem cap = Config.findWeaponByNBT(instance);
+        if (cap.isEmpty()) {
+            return instance.getCapability(CAPABILITY_ITEM);
+        } else {
+            return LazyOptional.of(() -> cap);
         }
-        CapabilityItem cap = Config.findWeaponByNBT(stack);
-        if(cap == CapabilityItem.EMPTY) {
-            cap = stack.getCapability(EpicFightCapabilities.CAPABILITY_ITEM, null).orElse(CapabilityItem.EMPTY);
-        }
-        cir.setReturnValue(cap);
     }
 
-    @Inject(at = @At(value = "HEAD"), method = "getItemStackCapabilityOr", cancellable = true)
-    private static void onGetItemStackCapabilityOr(ItemStack stack, CapabilityItem defaultCap, CallbackInfoReturnable<CapabilityItem> cir) {
-        if(stack.isEmpty()) {
-            cir.setReturnValue(defaultCap);
+    @Redirect(
+            method = "getItemStackCapabilityOr",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;getCapability(Lnet/minecraftforge/common/capabilities/Capability;)Lnet/minecraftforge/common/util/LazyOptional;")
+    )
+    private static LazyOptional<CapabilityItem> getFromConfigOr(ItemStack instance, Capability<CapabilityItem> capability) {
+        CapabilityItem cap = Config.findWeaponByNBT(instance);
+        if (cap.isEmpty()) {
+            return instance.getCapability(CAPABILITY_ITEM);
+        } else {
+            return LazyOptional.of(() -> cap);
         }
-        CapabilityItem cap = Config.findWeaponByNBT(stack);
-        if(cap == CapabilityItem.EMPTY) {
-            cap = stack.getCapability(EpicFightCapabilities.CAPABILITY_ITEM, null).orElse(CapabilityItem.EMPTY);
-        }
-        cir.setReturnValue(cap);
     }
 }
